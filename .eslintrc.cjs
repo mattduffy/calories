c2a0ab967c05514403bee15927edeb6a291708b1
Matthew Duffy <mattduffy@gmail.com>
2025-11-30 20:21:15 -0800

    Add dotfiles and update package.json with test script.

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  globals: {
+    window: true,
+    document: true,
+    origin: true,
+    worker: true,
+  },
+  env: {
+    es2021: true,
+    node: true,
+    browser: true,
+  },
+  plugins: [
+  ],
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      files: ["public/j/worker.js"],
+      rules: {
+        'no-restricted-globals': ['error', 'isFinite', 'isNaN'].concat(restrictedGlobals), 
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', {"code": 100}],
+    'new-cap': 'off',
+  },
+}
