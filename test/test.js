13884b117f2ea276ea155fe4eaa51ded4e1b0863
Matthew Duffy <mattduffy@gmail.com>
2026-05-04 21:26:17 -0700

    Add json data files to test runner.

--- test/test.js
+++ test/test.js
@@ -5,6 +5,16 @@ import {
   it,
 } from 'node:test'
 import assert from 'node:assert/strict'
+import {
+  calories,
+  simpleCalories,
+  pandolfCalories,
+} from '../src/index.js'
+import walk_1 from './walk_1.json' with { type: 'json' }
+import walk_2 from './walk_2.json' with { type: 'json' }
+
+console.log(walk_1.features[0].geometry.coordinates[0])
+console.log(walk_2.features[0].geometry.coordinates[0])
 
 const skip = { skip: true }
 
