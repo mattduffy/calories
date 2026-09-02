b697c70f6cf6b74e672b07afb25ae7afbae29c4d
Matthew Duffy <mattduffy@gmail.com>
2026-09-02 15:57:30 -0700

    Add unit tests for new walk_42 data file and include results in comparison table.

--- test/test.js
+++ test/test.js
@@ -66,8 +66,9 @@ import walk_38 from './walk_38-you-are-gonna-be-person.json' with { type: 'json'
 import walk_39 from './walk_39-hospitals-make-him-really-nervous.json' with { type: 'json' }
 import walk_40 from './walk_40-sit-back-and-enjoy-the-show.json' with { type: 'json' }
 import walk_41 from './walk_41-roasted-dicks-of-course.json' with { type: 'json' }
+import walk_42 from './walk_42-as-you-wee-around-the-room.json' with { type: 'json' }
 
-const latest = walk_41
+const latest = walk_42
 
 const __DEBUG__ = (process.env.NODE_ENV === 'development' || process.env.CALORIES_DEBUG)
 
@@ -4697,6 +4698,65 @@ describe('Calorie ensemble tests', async () => {
       simple1: _dot1(walk_41.features[0].properties.simpleCalories),
       simple2: _dot1(walk41Simple),
       pandolf1: _dot1(walk_41.features[0].properties.pandolfCalories.totalKcal),
+      pandolf2: _dot1(resultSet.pandolf.totalKcal),
+      lcda: _dot1(resultSet.lcda.totalKcal),
+      minMech: _dot1(resultSet.minMech.totalKcal),
+    })
+    const pKcal = resultSet.pandolf.totalKcal
+    const lKcal = resultSet.lcda.totalKcal
+    console.log(
+      `% diff ${Number.parseInt((Math.min(pKcal, lKcal) / Math.max(pKcal, lKcal)) * 100, 10)}%`,
+    )
+    assert(withinX(pKcal, lKcal, X))
+  })
+
+  it('calorieEnsemble test - walk_42', async () => {
+    console_log('')
+    console_log('calorie ensemble function test using walk_42 data file')
+    const coords = walk_42.features[0].geometry.coordinates
+    const date_42 = new Date(walk_37.features[0].properties.date)
+      .toLocaleDateString('en-US', {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric',
+      })
+    const weight = walk_42.features[0].properties.weights
+    const bodyW = _dot1(weight.body / 2.2)
+    const ruckW = _dot1(weight.ruck / 2.2)
+    const bmr = {
+      height: HEIGHT, weight: bodyW, age: AGE, sex: SEX,
+    }
+    const details = {
+      bodyWeightKg: bodyW,
+      loadKg: ruckW,
+      waterKg: 0,
+      terrain: 1.1,
+      smooth: SMOOTH,
+      smoothWindow: SMOOTH_WINDOW,
+      BMR: bmr,
+    }
+    const walk42Simple = simpleCalories(
+      m2m(walk_42.features[0].properties.duration),
+      {
+        body: _dot1(weight.body / 2.2),
+        ruck: _dot1(weight.ruck / 2.2),
+        water: 0,
+      },
+    )
+    const resultSet = calorieEnsemble(coords, details)
+    console_log(resultSet)
+    results.push({
+      date: date_42,
+      name: clipName(walk_42.features[0].properties.name),
+      distance: dist(walk_42.features[0].properties.distance),
+      duration: _dot1(m2m(walk_42.features[0].properties.duration)),
+      avgSpd: _dot1(resultSet.pandolf.avgSpeedMs),
+      weights: walk_42.features[0].weights = `b: ${_dot1(weight.body / 2.2)}, `
+        + `r: ${_dot1(weight.ruck / 2.2)}`,
+      apple: walk_42.features[0].properties.apple.activity,
+      simple1: _dot1(walk_42.features[0].properties.simpleCalories),
+      simple2: _dot1(walk42Simple),
+      pandolf1: _dot1(walk_42.features[0].properties.pandolfCalories.totalKcal),
       pandolf2: _dot1(resultSet.pandolf.totalKcal),
       lcda: _dot1(resultSet.lcda.totalKcal),
       minMech: _dot1(resultSet.minMech.totalKcal),
